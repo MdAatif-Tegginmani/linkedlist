578adb9772da34ff078f60702a9789d5d8c8956d
Md Aatif N Tegginmani <mdaatif84@gmail.com>
2022-12-13 23:23:07 +0530

    task16

--- linkedlist.js
+++ linkedlist.js
@@ -226,3 +226,22 @@ class Solution {
 
     }
 }
+
+
+// task 3
+
+var middleNode = function(head) {
+    if(head === null) return null;
+
+    let slowPointer = head;
+
+    let fastPointer = head;
+
+    while(fastPointer && fastPointer.next){
+
+        fastPointer = fastPointer.next.next;
+
+        slowPointer = slowPointer.next;
+
+    }
+}
